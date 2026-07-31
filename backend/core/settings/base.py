07245180dcaeb00334fd5cde740e701d86df133b
achangtroraleke <atachart.changtroraleke@gmail.com>
2026-07-31 19:10:53 -0400

    updated cors

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -96,7 +96,9 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
     ],
 }
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS=[
+"https://revenue-tracker-sample.netlify.app/"
+]
 
 MEDIA_URL="/media/"
 MEDIA_ROOT=BASE_DIR/"media"
