42d852f1dc11b5dbd9d15df8f0bf4bbd928fe129
achangtroraleke <atachart.changtroraleke@gmail.com>
2026-07-31 21:50:59 -0400

    fixed import error

--- backend/revenue/admin.py
+++ backend/revenue/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import RevenueEntry, UploadedFile
+from .models import RevenueEntry, UploadedFile
 # Register your models here.
 admin.site.register(RevenueEntry)
 admin.site.register(UploadedFile)
