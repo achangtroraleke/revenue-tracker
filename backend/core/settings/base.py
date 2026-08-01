85f1257448e8199d7561e7c240c941b7e988cd85
achangtroraleke <atachart.changtroraleke@gmail.com>
2026-07-31 21:05:02 -0400

    reconfig settings

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -95,6 +95,9 @@ REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny', # Allows requests without auth headers
+    ]
 }
 
 
